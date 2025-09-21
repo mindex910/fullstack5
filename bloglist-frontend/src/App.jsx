7fda9ebdb28b27180ef1b2dce78d9ee2eca6cc4f
Minttu Paalanen <minttupaalanen@gmail.com>
2025-09-21 18:46:55 +0300

    lil fix

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -63,6 +63,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
